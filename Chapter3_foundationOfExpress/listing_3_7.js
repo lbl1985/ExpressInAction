575ffc83ce3e2260fa3e504c8704ecadb86809e4
Binlong Li <bil@microsoft.com>
2017-02-28 23:01:39 -0800

    listing 3.7 finished.

---
+++ Chapter3_foundationOfExpress/listing_3_7.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.use(function(req, res, next){
+    console.log("In comes a " + req.method + " to " + req.url);
+    next();
+});
+
+app.use(function(req, res, next){
+    var min = (new Date()).getMinutes();
+    if(min % 2 === 0) {
+        next();
+    } else {
+        res.statusCode = 403;
+        res.end("Not authorized.");
+    }
+});
+
+app.use(function(req, res){
+    res.end("Secret info: the password is Swordfish! Yay~~");
+});
+
+http.createServer(app).listen(3000);
