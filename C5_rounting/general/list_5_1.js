fd32446508f44b1ff05ea907d76dc37787ac2e4c
Binlong Li <bil@microsoft.com>
2017-03-06 20:24:58 -0800

    listing 5-3 and 5-4

--- C5_rounting/general/list_5_1.js
+++ C5_rounting/general/list_5_1.js
@@ -5,14 +5,25 @@ app.get("/minjie", function(req, res){
     res.send("Welcome to minjie's homework!");
 });
 
-app.get("/users/:userid", function(req, res, next){
-    var id = parseInt(req.params.userid, 10);
-    if(id !== NaN){
-        res.send("Welcome to user page " + id.toString());
-    } else {
-        next();
-    }
+// app.get("/users/:userid", function(req, res, next){
+//     var id = parseInt(req.params.userid, 10);
+//     if(id !== NaN){
+//         res.send("Welcome to user page " + id.toString());
+//     } else {
+//         next();
+//     }
+// });
+
+app.get(/^\/users\/(\d+)$/, function(req, res){
+    var userId = parseInt(req.params[0], 10);
+    res.send("user page from regex " + userId.toString());
 });
+
+app.get(/^\/users\/(\d+)-(\d+)$/, function(req, res){
+    var startId = req.params[0];
+    var endId = req.params[1];
+    res.send("user page from " + startId.toString() + " to " + endId.toString());
+})
 
 app.use(function(req, res){
     res.status(404).send("Page not found!");
