7cb0f30bc62c13c6d0d69a8761b22ab1d32d6479
Binlong Li <bil@microsoft.com>
2017-03-08 06:16:16 -0800

    need more efforts for https, let's encrypt does not support non-linux like system...

--- C5_rounting/general/list_5_7.js
+++ C5_rounting/general/list_5_7.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var https = require("https");
 var path = require('path');
+var fs = require("fs");
 var apiRouter = require("./routes/api_router");
 
 var app = express();
@@ -14,5 +16,10 @@ app.use("/api", apiRouter);
 
 app.use(express.static(staticPath));
 app.use(express.static(photoPath));
+
+// var httpsOptions = {
+//     key: fs.readFileSync("./private/privatekey.pem"),
+//     cert: fs.readFileSync("./private/request.pem")
+// }
 
 app.listen(3000);
