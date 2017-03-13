57442bc81049182c36ccf07caa8f9619027f9204
Binlong Li <bil@microsoft.com>
2017-03-13 00:08:11 -0700

    sign up is mostly done

--- C8_database/learn-about-me/routes.js
+++ C8_database/learn-about-me/routes.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var User = require('./models/user');
 var router = express.Router();
+var passport = require("passport");
 
 router.use(function(req, res, next){
     res.locals.currentUser = req.user;
@@ -17,5 +18,35 @@ router.get("/", function(req, res, next){
         res.render("index", {users: users});
     });
 });
+
+router.get("/signup", function(req, res){
+    res.render("signup");
+});
+
+router.post("/signup", function(req, res, next){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    User.findOne({username: username}, function(err, user){
+        if(err) {return next(err); }
+        if(user) {
+            req.flash("error", "User already exists");
+            return res.redirect("/signup");
+        }
+        var newUser = new User({
+            username: username,
+            password: password
+        });
+        newUser.save();
+        return res.redirect("/");
+    });    
+});
+
+
+// router.use(passport.authenticate("login", {
+//             successRedict: "/",
+//             failureRedict: "/signup",
+//             failureFlash: true
+//         }));
 
 module.exports = router;
